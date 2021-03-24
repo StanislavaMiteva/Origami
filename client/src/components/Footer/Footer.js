f8e21f1289d7d446a75030e70a766b31907b3339
StanislavaMiteva <stanislava.miteva@gmail.com>
2021-03-24 18:48:48 +0200

    Insert key to FooterItem component.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,11 +6,14 @@ const Footer = () => {
     return (
         <footer className={style.footer}>
             <ul>
-                {FOOTER_CONSTANTS.map(x => 
-                    <FooterItem>
+                {FOOTER_CONSTANTS.map(x =>
+                    <FooterItem
+                        key={x.id}
+                        id={x.id}
+                    >
                         {x.text}
                     </FooterItem>
-                )}               
+                )}
                 <li className={style.listItem}>
                     <img src="/blue-origami-bird-flipped.png" alt="" />
                 </li>
